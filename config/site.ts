850e51704da23f3908a5e851049e2501537cd79c
Mike <you@example.com>
2024-08-09 11:46:08 +0530

    title

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "MetaGlobal",
+  description: "MetaGlobal your technology partner",
   navItems: [
     {
       label: "Home",
