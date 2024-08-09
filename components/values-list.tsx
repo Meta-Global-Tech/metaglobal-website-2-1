67de74b552d86df0efe08a4aebb197c528fce6f7
Mike <you@example.com>
2024-08-09 11:51:58 +0530

    f

--- components/values-list.tsx
+++ components/values-list.tsx
@@ -11,7 +11,7 @@ import {Spacer} from "@nextui-org/react";
 // import { useRouter } from 'next/router';
 
 export const ValueList = ()=>{
-  const basePath = ''
+  const basePath = 'metaglobal-website-2-1'
   return (
     <div className="text-center justify-center">
     <ul className="text-center justify-center space-y-4">
